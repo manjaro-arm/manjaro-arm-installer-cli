c854feb1c3b458d29bfa1ad6de526e5f31613e02
Taha <lsdjkflfdskj@dlksjdljkfds.com>
2016-02-09 20:51:40 -0500

    Switched to cli project; added more documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,6 @@
-#include "mainwindow.h"
-#include <QApplication>
+#include <iostream>
 
-int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    return a.exec();
+int main(int argc, char** argv) {
+    std::cout << "Hi";
+    return 0;
 }
