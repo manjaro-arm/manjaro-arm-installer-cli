42133f21337ad8fd934363a8be3303009a33cecf
Taha <lsdjkflfdskj@dlksjdljkfds.com>
2016-02-09 21:04:36 -0500

    Forgot to add bin/ folder

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <unistd.h> // for chroot
+#include <cerrno> // errno variable
 
 int main(int argc, char** argv) {
-    std::cout << "Hi";
+    std::cout << "Note: this program must be run as root\n";
+    int result = chroot("/home/owner/Downloads/chroot-env/");
+
+    if (result == 0) {
+        std::cout << "Chroot success." << std::endl;
+    } else {
+        std::cout << "Chroot failure: " << result << " " << errno << std::endl;
+    }
     return 0;
 }
